0d2fbe20287c53e4568edc4a33680cc61e04310b
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-01-08 16:51:31 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 320 ms, Memory: 18180 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+        for(int i=0; i<N; i++)
+        {
+            for(int z = 0; z < N-i-1; z++)
+            {
+                System.out.print(' ');
+            }
+            for(int j = 0; j<2*(i)+1; j++)
+            {
+                System.out.print('*');
+            }
+
+            System.out.println();
+        }
+    }
+}
