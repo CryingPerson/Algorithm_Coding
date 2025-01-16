ed0e587f27597add185e11afd179942360d2cf30
CryingPerson <opnice12@naver.com>
2025-01-16 16:13:41 +0900

    알고리즘

--- 백준/Silver/2346. 풍선 터트리기/카드2.java
+++ 백준/Silver/2346. 풍선 터트리기/카드2.java
@@ -1,27 +1,52 @@
-import com.sun.jdi.Value;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-
+class EVENT1
+{
+    int index;
+    int value;
+    EVENT1(int index, int value)
+    {
+        this.index = index;
+        this.value = value;
+    }
+}
 public class Main {
-    static Deque<Integer> deque;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str;
+        StringBuilder sb = new StringBuilder();
+        Deque<EVENT1> deque = new ArrayDeque<>();
         int N = Integer.parseInt(br.readLine());
-
-        Queue<Integer> queue = new LinkedList<>();
-
-        for(int i = 1; i<=N; i++)
-        {
-            queue.add(i);
+        str = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            deque.add(new EVENT1(i,Integer.parseInt(str.nextToken())));
         }
-
-        while (queue.size() > 1)
+        while (deque.size()>1)
         {
-            queue.poll();
-            queue.add(queue.poll());
+            sb.append(deque.getFirst().index+" ");
+            int value = deque.poll().value;
+
+
+            if(value > 0)
+            {
+                for(int i=0; i<value-1; i++)
+                {
+                    deque.addLast(deque.pollFirst());
+                }
+
+            }
+            else
+            {
+                for(int i=0; i<Math.abs(value); i++)
+                {
+                    deque.addFirst(deque.pollLast());
+                }
+
+            }
         }
-        System.out.println(queue.poll());
+        sb.append(deque.poll().index);
+        System.out.println(sb);
+
     }
 }
