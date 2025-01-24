b7a654fc4ef4e4d017f13b18f5a48c51b7fbabfc
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-01-24 18:45:31 +0900

    [level 2] Title: 기능개발, Time: 3.44 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,18 +5,17 @@ class Solution {
         
         Queue<Integer> q = new LinkedList();
         List<Integer> li = new ArrayList();
-        for(int i = 0; i<progresses.length; i++){
+        for(int i = 0; i < progresses.length; i++){
             int days = 100 - progresses[i];
             
             int time = (int)Math.ceil((double)days / speeds[i]);
             q.add(time);
         }
         
-       
         while(!q.isEmpty()){
             int cnt = 1;
-            int current = q.poll();
-            while(!q.isEmpty() &&q.peek() <= current){
+            int cur = q.poll();
+            while(!q.isEmpty() && q.peek() <= cur){
                 q.poll();
                 cnt++;
             }
