402e36da24ca199deca4ba28671cf85e9729fa43
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-06 17:30:08 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 168 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,5 +1,4 @@
 
-import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
@@ -7,33 +6,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int[] arr = new int[num];
-        for(int i = 0; i<arr.length; i++){
+        for(int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        for(int x : Solution.solution(arr))
+        for(int x : Solution.solution(arr)) {
             System.out.println(x);
+        }
     }
 }
-class Solution{
-    public static int[] solution(int[] n){
-        int[] dx = {1,2,3};
-        int answer = 0;
+
+class Solution {
+    public static int[] solution(int[] n) {
         int[] result = new int[n.length];
-        for(int i = 0; i<n.length; i++){
-            result[i] = DFS(0, n[i], dx);
+        for (int i = 0; i < n.length; i++) {
+            int[] dp = new int[n[i] + 1];
+
+            // 초기값 설정
+            if (n[i] >= 0) dp[0] = 1;
+            if (n[i] >= 1) dp[1] = 1;
+            if (n[i] >= 2) dp[2] = 2;
+
+            // DP 계산
+            for (int j = 3; j <= n[i]; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+            }
+
+            result[i] = dp[n[i]];  // n[i]에 해당하는 값 저장
         }
         return result;
-    }
-    public static int DFS(int answer,int n, int[] dx){
-        int cnt = 0;
-        for(int i =0; i<dx.length; i++){
-            if(answer > n) return 0;
-            if(answer == n){
-                return 1;
-            }else{
-                cnt += DFS(answer + dx[i],n, dx);
-            }
-        }
-        return cnt;
     }
 }
