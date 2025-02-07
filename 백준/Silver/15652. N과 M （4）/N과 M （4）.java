9116b8f63baad79f01cc0cb8da7dd95fdb8cc130
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-08 00:40:28 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 17416 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+class node{
+    int x;
+    int y;
+    public node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    static int[] ch;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer str = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(str.nextToken());
+        int m = Integer.parseInt(str.nextToken());
+        ch = new int[n+1];
+        int[] arr = new int[m+1];
+        solution(n,m,0,1,arr,ch);
+        System.out.println(sb);
+    }
+    public static void solution(int n, int m, int L, int s ,int []arr, int[] ch) {
+        if(L == m){
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+        }else{
+            for(int i = s; i<=n; i++){
+                arr[L] = i;
+                solution(n,m,L+1,i,arr,ch);
+            }
+        }
+    }
+
+}
