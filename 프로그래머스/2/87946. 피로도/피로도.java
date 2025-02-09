d310b3d552087da24e7916ac86e2ce4db579e03e
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-09 22:16:45 +0900

    [level 2] Title: 피로도, Time: 17.16 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,26 @@
 class Solution {
-    static int[] arr;
-    static int ans;
+    static int ans = 0;
+    static int[] ch;
     public int solution(int k, int[][] dungeons) {
         int answer = 0;
-        ans = 0;
-        arr = new int[dungeons.length];
-        DFS(k, dungeons, answer);
+        ch = new int[dungeons.length];
+        for(int i = 0; i<dungeons.length; i++){
+            DFS(k,0,dungeons,answer);
+        }
+        
         return ans;
     }
-    public static void DFS(int k, int[][]dungeons, int answer){
-        
+    public static void DFS(int n ,int L, int[][] dungeons, int answer){
         ans = Math.max(ans, answer);
+        
+    
             for(int i = 0; i<dungeons.length; i++){
-                if(arr[i] == 0 && k >= dungeons[i][0]){
-                    arr[i] = 1;
-            
-                    DFS(k - dungeons[i][1], dungeons, answer+1);
-                    arr[i] = 0;
+                if(dungeons[i][0] <= n && ch[i] == 0) {
+                    ch[i] = 1;
+                    DFS(n - dungeons[i][1], L+1, dungeons, answer+1);
+                    ch[i] = 0;
                 }
-            }
-      }
+    
+        }
+    }
 }
