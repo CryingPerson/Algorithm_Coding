8e582229c0b0aaa3b22789d1adf2476d2ec45840
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-10 22:06:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 204 ms, Memory: 21924 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,45 +1,60 @@
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    static int[] ch;  // 방문 체크 배열
-
+    static int[] ch;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();  // 현재 위치
-        int m = sc.nextInt();  // 목표 위치
-        ch = new int[100001];  // 최대 범위 100000까지
-
+        int second = 1;
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        ch = new int[100001];
+        ch[n] = 1;
         Queue<Integer> queue = new LinkedList<>();
         queue.add(n);
-        ch[n] = 1;  // 방문 체크 (1부터 시작)
-
-        int answer = 0;  // 이동 횟수
-
+        int answer = 0;
+       int da = 0; int db = 0; int dc = 0;
         while (!queue.isEmpty()) {
+
             int size = queue.size();
-            
-            for (int i = 0; i < size; i++) {
+            for(int i = 0; i<size; i++){
                 int poll = queue.poll();
-
-                // 목표 위치 도착 시 즉시 출력 후 종료
-                if (poll == m) {
+                if(poll == m) {
                     System.out.println(answer);
-                    return;  // 프로그램 종료
+                    return;
                 }
-
-                int[] next = {poll - 1, poll + 1, poll * 2};
-
-                for (int nx : next) {
-                    if (nx >= 0 && nx <= 100000 && ch[nx] == 0) {
-                        ch[nx] = 1;  // 방문 체크
-                        queue.add(nx);
+                da = poll - 1;
+                db = poll + 1;
+                dc = poll * 2;
+                if (da == m) {
+                    System.out.println(answer+1);
+                    return;
+                } else if (db == m) {
+                    System.out.println(answer+1);
+                    return;
+                } else if (dc == m) {
+                    System.out.println(answer+1);
+                    return;
+                } else {
+                    if(da >= 0 && da <=100000 && ch[da] == 0){
+                        ch[da] = 1;
+                        queue.add(da);
+                    }
+                    if(db >= 0 && db <=100000 && ch[db] == 0){
+                        ch[db] = 1;
+                        queue.add(db);
+                    }
+                    if(dc >= 0 && dc <=100000 && ch[dc] == 0){
+                        ch[dc] = 1;
+                        queue.add(dc);
                     }
                 }
             }
-            answer++;  // BFS의 레벨 증가 (시간 증가)
+            answer++;
         }
+        System.out.println(-1);
     }
 }
