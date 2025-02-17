107a5b84b72d06f278bb74706566dbb2d164c316
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-17 20:51:25 +0900

    [level 1] Title: 신고 결과 받기, Time: 188.88 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,38 +2,31 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
-        Map<String, HashMap<String, Integer>> map = new HashMap<>();
-        Map<String, Integer> idxMap = new HashMap<>();
-
-        // 초기화
-        for (int i = 0; i < id_list.length; i++) {
+        HashMap<String, HashMap<String, Integer>> map = new HashMap();
+        HashMap<String, Integer> ma = new HashMap();
+        for(int i = 0; i<id_list.length; i++){
+        
             String name = id_list[i];
-            map.put(name, new HashMap<>());
-            idxMap.put(name, i);
+            map.put(name, new HashMap());
+            ma.put(name,i);
         }
-
-        // report를 기반으로 map 업데이트
-        for (String s : report) {
-            String[] str = s.split(" ");
-            String from = str[0];
-            String to = str[1];
-
-            // 신고를 처음 하면 1로 초기화, 이미 했다면 아무 변화 없음
-            map.get(to).put(from, map.get(to).getOrDefault(from, 0) + 1);
+        for(String s : report){
+            String[] st = s.split(" ");
+            String reports = st[0];
+            String reporteds = st[1];
+            
+            map.get(reporteds).put(reports,1);
         }
-
-        // k번 이상 신고된 경우 처리
-        for (int i = 0; i < id_list.length; i++) {
-            String reported = id_list[i];
-            HashMap<String, Integer> senders = map.get(reported);
-
-            if (senders.size() >= k) {
-                for (String sender : senders.keySet()) {
-                    answer[idxMap.get(sender)]++;
+        
+        for(int i = 0; i<id_list.length; i++){
+            String repoted = id_list[i];
+            HashMap<String, Integer> maps = map.get(repoted);
+            if(maps.size() >= k){
+                for(String se : maps.keySet()){
+                    answer[ma.get(se)]++;
                 }
             }
         }
-
         return answer;
     }
 }
