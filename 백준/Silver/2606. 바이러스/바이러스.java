afa9cb96cf1043852496aa208a19e5aa9bb906fd
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-18 15:02:22 +0900

    [Silver III] Title: 바이러스, Time: 188 ms, Memory: 17776 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,47 +1,55 @@
-
+import java.lang.reflect.Array;
 import java.util.*;
 
-class Main {
-    static int[] dx = {-1, 0, 1, 0}; //
-    static  ArrayList<ArrayList<Integer>> list;
-    static int[] dy = {0, 1, 0, -1};
+public class Main {
+    static int INF = Integer.MAX_VALUE;
     static int[] ch;
+    static ArrayList<ArrayList<Integer>> list;
+    static int m;
+    static int[] dis;
+    static int answer = 0;
+    static int[] dx = {-1, 1};
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int m = kb.nextInt();
         ch = new int[n+1];
         list = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
-
             list.add(new ArrayList<>());
         }
         for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-
+            int a = kb.nextInt();
+            int b = kb.nextInt();
             list.get(a).add(b);
             list.get(b).add(a);
-        }
-        int cnt = 0;
-        int bfs = BFS(1, cnt);
-        System.out.println(bfs);
-    }
 
-    public static int BFS( int n, int cnt) {
+        }
+        BFS(1);
+        System.out.println(answer);
+    }
+    public static void BFS(int n) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(n);
-        ch[1]= 1;
+        ch[n] = 1;
+
         while (!queue.isEmpty()) {
-            Integer poll = queue.poll();
-            cnt++;
-            for(int x : list.get(poll)){
-                if(ch[x] != 1){
-                    ch[x] = 1;
-                    queue.add(x);
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int poll = queue.poll();
+
+                for (int nx : list.get(poll)) {
+                    if(ch[nx] == 0){
+                        ch[nx] = 1;
+                        answer++;
+                        queue.add(nx);
+                    }
                 }
             }
         }
-        return cnt-1;
     }
 }
+
