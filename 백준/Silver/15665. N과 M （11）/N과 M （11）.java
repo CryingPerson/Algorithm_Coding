5f829a56d73d42a29baa90d260960b2751c3abea
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-20 15:18:59 +0900

    [Silver II] Title: N과 M (11), Time: 544 ms, Memory: 59732 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+
+public class Main {
+    static int INF = Integer.MAX_VALUE;
+    static int[] ch;
+    static ArrayList<ArrayList<Integer>> list;
+    static int m;
+    static int n;
+    static int[][] board;
+    static int[][] dis;
+    static int[] answer;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        n = kb.nextInt();
+        m = kb.nextInt();
+        ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = kb.nextInt();
+        }
+        Arrays.sort(ans);
+        ch = new int[n];
+
+        answer = new int[m];
+        int L = 0;
+        DFS(L);
+
+        System.out.println(sb);
+    }
+
+    public static void DFS(int L) {
+        if (L == m) {
+            for (int x : answer) sb.append(x).append(" ");
+            sb.append("\n");
+            return;
+        }
+        Set<Integer> set = new HashSet<>();
+//        Set<Integer> set = new HashSet<>();
+//        for (int i = s; i < n; i++) {
+//            if(!set.contains(ans[i])){
+//
+//                answer[L] = ans[i];
+//                set.add(ans[i]);
+//                DFS(L+1, i);
+//            }
+//        }
+        for (int i = 0; i < n; i++) {
+            if(!set.contains(ans[i])){
+
+                answer[L] = ans[i];
+                set.add(ans[i]);
+                DFS(L+1);
+            }
+        }
+    }
+
+
+}
+
