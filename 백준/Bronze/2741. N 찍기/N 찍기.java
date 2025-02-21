23e99e2b04581e3e65ef58dad4b40a1d2ccaeb19
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-21 19:46:16 +0900

    [Bronze V] Title: N 찍기, Time: 832 ms, Memory: 32684 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class Main {
+    static int INF = Integer.MAX_VALUE;
+    static int[] ch;
+    static ArrayList<ArrayList<Integer>> list;
+    static int m;
+    static int n;
+    static int[][] board;
+    static int[][] dis;
+    static int[] answer;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] ans;
+    static int zero = 0;
+    static int one = 0;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+
