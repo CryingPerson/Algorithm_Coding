0e24d428f3c9b658396a7bb9bb934424611030f4
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-24 16:00:35 +0900

    [Silver IV] Title: 수들의 합 2, Time: 328 ms, Memory: 29172 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,48 @@
+
+import java.awt.*;
+import java.util.*;
+
+public class Main {
+    static int INF = Integer.MAX_VALUE;
+    static int[] ch;
+    static ArrayList<ArrayList<Integer>> list;
+    static int m;
+    static int n;
+    static int[][] board;
+    static int[][] dis;
+    static int[] answer;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] ans;
+    static int zero = 0;
+    static int one = 0;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        long m = kb.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        int left = 0; int right = 0; int sum = 0; int cnt = 0;
+        while (right < n){
+            sum += arr[right];
+
+            while (sum > m){
+                sum -= arr[left++];
+            }
+            if(sum == m) cnt++;
+
+            right++;
+        }
+        System.out.println(cnt);
+    }
+
+}
+
