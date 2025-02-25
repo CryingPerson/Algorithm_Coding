61c1368e80962cfb9241d6fbbe89a5d9eb9563c5
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-25 13:37:26 +0900

    [Bronze V] Title: 검증수, Time: 168 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += (int) Math.pow(sc.nextInt(), 2);
+        }
+        System.out.println(sum % 10);
+    }
+}
