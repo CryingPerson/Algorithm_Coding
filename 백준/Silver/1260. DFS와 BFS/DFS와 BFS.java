b4ed4ba12a08fa97dc556e6c176d85ccf8eb67b9
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-25 17:26:20 +0900

    [Silver II] Title: DFS와 BFS, Time: 508 ms, Memory: 39688 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,86 +1,58 @@
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-class node{
-    int x;
-    int y;
-    public node(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
+
 public class Main {
-    public static void main(String[] args) {
-
+    static ArrayList<ArrayList<Integer>> list1;
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-
-        int point = sc.nextInt();
-        int line = sc.nextInt();
-        int start = sc.nextInt();
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for(int i = 0; i<=point; i++){
-            list.add(new ArrayList<>());
+        ArrayList<int[]> list = new ArrayList<>();
+         list1 = new ArrayList<>();
+        int n = sc.nextInt(); int m =sc.nextInt(); int start = sc.nextInt();
+        int[] ch = new int[n+1];
+        int[] ch2 = new int[n+1];
+        ch[start] = 1;
+        ch2[start] = 1;
+        for (int i = 0; i <= n; i++) {
+            list1.add(new ArrayList<>());
         }
-        for(int i = 0; i<line; i++){
+        for (int i = 0; i < m; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
 
-            list.get(a).add(b);
-            list.get(b).add(a);
-
+            list1.get(a).add(b);
+            list1.get(b).add(a);
         }
-        for(ArrayList<Integer> ed : list){
-            Collections.sort(ed);
+        for(ArrayList<Integer> list2 : list1){
+            Collections.sort(list2);
         }
-        int[] solution = Solution.solution(point, list, start);
-        for(int x : Solution.DFS(point, list, start)) {
-            System.out.print(x + " ");
-        }
+        DFS(start, ch);
         System.out.println();
-
-        for(int x : Solution.solution(point, list, start)){
-            System.out.print(x + " ");
-        }
+        BFS(start, ch2);
     }
 
-}
-class Solution {
-    static int[] che;
-    static List<Integer> result;
-    public static int[] solution(int n, ArrayList<ArrayList<Integer>> list,int start) {
-
-        Queue<Integer> queue = new LinkedList<>();
-        int[] ch = new int[n+1];
-        ch[start] = 1;
-        queue.add(start);
-        List<Integer> list1 = new ArrayList<>();
-        while (!queue.isEmpty()){
-            int poll = queue.poll();
-            list1.add(poll);
-            for(int nv : list.get(poll)){
-                if(ch[nv] == 0){
-                    ch[nv] = 1;
-                    queue.add(nv);
-                }
+    public static void DFS(int start, int[]ch) {
+        System.out.print(start + " ");
+        for(int x : list1.get(start)){
+            if(ch[x] == 0){
+                ch[x] = 1;
+                DFS(x, ch);
             }
         }
-
-        return list1.stream().mapToInt(i -> i).toArray();
     }
-    public static int[] DFS(int n, ArrayList<ArrayList<Integer>> list,int start){
-       result = new ArrayList<>();
-       che = new int[n+1];
 
-        DF(list, start);
-
-        return result.stream().mapToInt(i -> i).toArray();
-    }
-    public static void DF(ArrayList<ArrayList<Integer>> list,int start){
-        che[start] = 1;
-        result.add(start);
-
-        for(int nx : list.get(start)){
-            if(che[nx] == 0){
-                DF(list,nx);
+    public static void BFS(int start, int[] ch2) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        ch2[start] = 1;
+        while (!queue.isEmpty()){
+            Integer poll = queue.poll();
+            System.out.print(poll + " ");
+            for(int x : list1.get(poll)){
+                if(ch2[x] == 0){
+                    ch2[x] = 1;
+                    queue.add(x);
+                }
             }
         }
     }
