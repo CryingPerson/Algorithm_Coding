96532ddd98229f5c63ebd2b7f06f5b4470385411
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-27 14:08:16 +0900

    [Silver III] Title: 계단 오르기, Time: 188 ms, Memory: 17996 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,41 +1,45 @@
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
-    public static void main(String[] args) {
-
+    static ArrayList<ArrayList<Integer>> list1;
+    static int n, m;
+    static int[][] board;
+    static int MaxSave = -1;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int area = -1;
+    static int[] ch;
+    static int[] paraent;
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-
+        list1 = new ArrayList<>();
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
-        int solution = Solution.solution(arr);
-        System.out.println(solution);
-    }
 
-}
-class Solution {
-    public static int solution(int[] arr) {
-        if (arr.length == 1) {
-            return arr[0];
+        // 첫번쨰 두번쨰 네번쨰 VS 첫번쨰 세번쨰 네번쨰 중 큰걸 찾아야함
+        // 구조적으로 두번쨰부터 시작하면 위에꺼보다 클수가 없으니 제외하고 시작하자
+        int[] dp = new int[n];
+        int[]stairs = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = sc.nextInt();
+            stairs[i] = dp[i];
         }
-        if (arr.length == 2) {
-            return arr[0] + arr[1]; // 두 계단만 있을 경우 직접 합산
+        if(n == 1){
+            System.out.println(dp[0]);
+            return;
         }
-        int[] dp = new int[arr.length];
-        int[] staris = new int[arr.length];
-        for(int i = 0; i<arr.length; i++){
-            staris[i] = arr[i];
+        if(n== 2){
+            System.out.println(dp[0] + dp[1]);
+            return;
         }
-        dp[0] = arr[0];
-        dp[1] = arr[1]+ arr[0];
-        dp[2] = Math.max(arr[1]+arr[2], arr[0]+ arr[2]);
-
-        for(int i = 3; i<arr.length; i++){
-            dp[i] = Math.max(dp[i-3] + staris[i-1] + staris[i], dp[i-2] + staris[i]);
+        dp[0] = stairs[0];
+        dp[1] = stairs[0] + stairs[1];
+        dp[2] = Math.max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
+        for (int i = 3; i < n; i++) {
+            dp[i] = Math.max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2]+ stairs[i]);
         }
-        return dp[arr.length-1];
+        System.out.println(dp[n-1]);
     }
 }
+
