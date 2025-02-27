b83ddebec59302491545b51ff668f3cc9b0b1fec
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-27 14:38:28 +0900

    [Silver III] Title: 1로 만들기, Time: 196 ms, Memory: 21620 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,33 @@
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[1] = 0; // 1은 연산 필요 없음
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1; // 1 빼기 연산
-            
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 2로 나누기
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 3으로 나누기
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int solution = Solution.solution(n);
+        System.out.println(solution);
+    }
+
+}
+class Solution {
+    public static int solution(int arr) {
+        if(arr == 1) return 0;
+        if(arr == 2) return 1;
+
+        int[] dp = new int[arr+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[1] = 0;
+        dp[2] = 1;
+
+        for(int i =3; i<=arr; i++){
+            if(i % 2 == 0) dp[i] = dp[i/2] + 1;
+            if(i % 3 == 0) dp[i] = Math.min(dp[i/3] + 1, dp[i]);
+            dp[i] = Math.min(dp[i-1] + 1, dp[i]);
         }
-        
-        System.out.println(dp[n]); // 최소 연산 횟수 출력
+
+        return dp[arr];
     }
 }
