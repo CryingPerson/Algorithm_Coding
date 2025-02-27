10cf04c7c93ad951c28d39eda0cc3fbde8c05f03
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-28 00:13:48 +0900

    [level 2] Title: 비밀 코드 해독, Time: 57.60 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    static int[] arr;
+    static int answer;
+    public int solution(int n, int[][] q, int[] ans) {
+         answer = 0;
+        
+        
+        arr = new int[5];
+        DFS(n,q,0,1, ans);
+        return answer;
+    }
+    public static void DFS(int n,int[][] q,int L, int s, int[] ans){
+        if(L == 5){
+            if(check(q,ans)){
+                answer++;
+            }
+            return;
+        }
+        for(int i = s; i<=n; i++){
+            arr[L] = i;
+            DFS(n,q, L+1, i+1,ans);
+        }
+    }
+    public static boolean check(int[][]q, int[]ans){
+        Set<Integer> set = new HashSet();
+        for(int x : arr) set.add(x);
+        for(int i = 0; i < q.length; i++){
+            int cnt = 0;
+            for(int j = 0; j < q[i].length; j++){
+                if(set.contains(q[i][j])){
+                    cnt++;
+                }
+            }
+            if(ans[i] != cnt){
+                return false;
+            }
+        }
+        return true;
+    }
+}
