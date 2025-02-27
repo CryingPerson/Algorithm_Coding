942d83429e209117f3b442d224793e71ba189ef7
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-27 16:03:14 +0900

    [Silver I] Title: 정수 삼각형, Time: 780 ms, Memory: 132500 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,40 +1,59 @@
-
-
-import java.lang.reflect.Array;
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    static ArrayList<ArrayList<Integer>> list1;
+    static int n, m;
+    static int[][] board;
+    static int MaxSave = -1;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int area = -1;
+    static int[] ch;
+    static int[] paraent;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner kb = new Scanner(System.in);
-
-        int n = kb.nextInt();
-
-        int[][] tringle = new int[n][n];
-
-        int[][] dp = new int[n][n];
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                tringle[i][j] = kb.nextInt();
+                arr[i][j] = sc.nextInt();
             }
         }
-        dp[0][0] = tringle[0][0];
-        int max = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if(j == 0){
-                    dp[i][j] = tringle[i][j] + dp[i-1][j];
-                }else if(i == j){
-                    dp[i][j] = tringle[i][j] + dp[i-1][j-1];
+        int[][]dp = new int[n][n];
+        if(n== 1){
+            System.out.println(arr[0][0]);
+            return;
+        }
+        int see = 0;
+        if(n== 2){
+            see = Math. max(arr[0][0]+arr[1][0], arr[0][0] + arr[1][1]);
+            System.out.println(see);
+            return;
+        }
+        dp[0][0] = arr[0][0];
+        dp[1][0] = arr[0][0] + arr[1][0];
+        dp[1][1] = arr[0][0] + arr[1][1];
+
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if(j == 0) {
+                    dp[i][j] = dp[i-1][j] + arr[i][j];
+                }
+                else if(j == i) {
+                    dp[i][j] = dp[i-1][j-1] + arr[i][j];
                 }else{
-                    dp[i][j] = tringle[i][j] + Math.max(dp[i-1][j-1],dp[i-1][j]);
+                    dp[i][j] = Math.max(dp[i-1][j-1] + arr[i][j], dp[i-1][j] + arr[i][j]);
                 }
             }
         }
+        int result = 0;
         for (int i = 0; i < n; i++) {
-            max = Math.max(dp[n-1][i], max);
+            result = Math.max(dp[n-1][i],result);
         }
-        System.out.println(max);
+        System.out.println(result);
     }
-
 }
+
