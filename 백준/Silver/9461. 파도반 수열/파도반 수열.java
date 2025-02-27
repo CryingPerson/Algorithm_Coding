752cbc074a506525689274caffbc0fa2f5cca3a2
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-27 16:23:35 +0900

    [Silver III] Title: 파도반 수열, Time: 188 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,30 +1,42 @@
-
-
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
-class Main {
-    static int[] dx = {-1, 0, 1, 0}; //
-    static  ArrayList<ArrayList<Integer>> list;
+public class Main {
+    static ArrayList<ArrayList<Integer>> list1;
+    static int n, m;
+    static int[][] board;
+    static int MaxSave = -1;
+    static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static int area = -1;
     static int[] ch;
-    public static void main(String[] args) {
+    static int[] paraent;
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+        long[]dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        for (int i = 4; i <= 100; i++) {
+            dp[i] = dp[i-3] + dp[i-2];
+        }
         for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            long[] d = new long[a+1];
-            long dp = dp(a, d);
-            System.out.println(dp);
+            int num = sc.nextInt();
+            if(num == 1 || num == 2 || num == 3){
+                if(num == 1){
+                    System.out.println(1);
+                }else if(num == 2){
+                    System.out.println(1);
+                }else{
+                    System.out.println(1);
+                }
+            }else{
+                System.out.println(dp[num]);
+            }
         }
-    }
-    public static long dp(int a, long[] d){
-        if(a == 0 || a == 1 || a == 2) return 1;
-         d[1] = 1;
-         d[2] = 1;
-         d[3] = 1;
-        for (int i = 3; i <= a; i++) {
-            d[i] = d[i-3] + d[i-2];
-        }
-        return d[a];
     }
 }
+
