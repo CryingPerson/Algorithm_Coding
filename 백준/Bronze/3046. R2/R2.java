1644c4c4a2b3fa3c0c458e1105358735cd1f0d93
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-02-28 18:36:34 +0900

    [Bronze IV] Title: R2, Time: 172 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int a = 0;
+
+        System.out.println(m * 2 - n);
+    }
+}
