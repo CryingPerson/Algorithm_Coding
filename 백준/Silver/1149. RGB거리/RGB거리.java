4f2399c9559b6e0e31060b26f3ca3e14266cf03c
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-03-03 16:59:25 +0900

    [Silver I] Title: RGB거리, Time: 232 ms, Memory: 20088 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,45 +1,42 @@
-
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    static int INF = Integer.MAX_VALUE;
-    static int[] ch;
-    static ArrayList<ArrayList<Integer>> list;
-    static int m;
-    static int n;
-    static int[][] board;
-    static int[][] dis;
-    static int[] answer;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] ans;
-    static int zero = 0;
-    static int one = 0;
-    static StringBuilder sb = new StringBuilder();
+    public static int[]dx = {-1,0,1,0};
+    public static int[]dy = {0,1,0,-1};
+    public static int n;
+    public static int[]ch;
+    public static char[][] color;
+    public static char[][] color2;
+    public static ArrayList<ArrayList<Integer>> list;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+        int T = sc.nextInt();
 
-        Scanner kb = new Scanner(System.in);
-
-        int n = kb.nextInt();
-        int[][]dp = new int[n][n];
-        int[][] arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = kb.nextInt();
-            arr[i][1] = kb.nextInt();
-            arr[i][2]= kb.nextInt();
+        int[][] dp = new int[T][3];
+        int[][] home = new int[T][3];
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < 3; j++) {
+                home[i][j] = sc.nextInt();
+            }
         }
-        dp[0][0] = arr[0][0];
-        dp[0][1] = arr[0][1];
-        dp[0][2] = arr[0][2];
+        dp[0][0] = home[0][0];
+        dp[0][1] = home[0][1];
+        dp[0][2] = home[0][2];
 
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = arr[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);
-            dp[i][1] = arr[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
-            dp[i][2] = arr[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+        for (int i = 1; i < T; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(j == 0){
+                    dp[i][j] = Math.min(dp[i-1][j+1], dp[i-1][j+2]) + home[i][j];
+                }else if(j == 2){
+                    dp[i][j] = Math.min(dp[i-1][j-2], dp[i-1][j-1]) + home[i][j];
+                }else{
+                    dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j+1]) + home[i][j];
+                }
+            }
         }
-        System.out.println(Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2])));
+        System.out.println(Math.min(dp[T-1][0], Math.min(dp[T-1][1],dp[T-1][2])));
     }
-
 }
-
