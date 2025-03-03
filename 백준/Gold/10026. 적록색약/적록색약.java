313da1f0d6e9d487043b2d95278d28df150ce3fa
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-03-03 15:15:10 +0900

    [Gold V] Title: 적록색약, Time: 216 ms, Memory: 19204 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -18,65 +18,41 @@ public class Main {
         for (int i = 0; i < n; i++) {
             color[i] = sc.nextLine().toCharArray();
         }
-        for (int i = 0; i < n; i++) {
-            color2[i] = color[i].clone();
-        }
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(Character.isAlphabetic(color[i][j])){
+                if(color2[i][j] == 0){
                     cnt++;
                     char ch = color[i][j];
                     DFS(i,j, ch);
                 }
+                if(color[i][j] == 'G'){
+                    color[i][j] = 'R';
+                }
             }
         }
         System.out.print(cnt + " ");
+        color2 = new char[n][n];
         int snt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(Character.isAlphabetic(color2[i][j])){
+                if(color2[i][j] == 0){
                     snt++;
-                    char ch = color2[i][j];
-                    DFS2(i,j, ch);
+                    char ch = color[i][j];
+                    DFS(i,j, ch);
                 }
             }
         }
         System.out.print(snt);
     }
     public static void DFS(int x, int y, char cs){
-        color[x][y] = 1;
+        color2[x][y] = 1;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if(nx >=0 && nx < n && ny >= 0 && ny < n && color[nx][ny] == cs){
-                color[nx][ny] = 1;
+            if(nx >=0 && nx < n && ny >= 0 && ny < n && color[nx][ny] == cs && color2[nx][ny] == 0){
                 DFS(nx,ny,cs);
-            }
-        }
-    }
-    public static void DFS2(int x, int y, char cs){
-        color2[x][y] = 1;
-        if(cs == 'R' || cs == 'G'){
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(nx >=0 && nx < n && ny >= 0 && ny < n && (color2[nx][ny] == 'R' || color2[nx][ny] == 'G')){
-                    color2[nx][ny] = 1;
-                    DFS2(nx,ny,cs);
-                }
-            }
-        }else{
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(nx >=0 && nx < n && ny >= 0 && ny < n && color2[nx][ny] == cs){
-                    color2[nx][ny] = 1;
-                    DFS2(nx,ny,cs);
-                }
             }
         }
     }
