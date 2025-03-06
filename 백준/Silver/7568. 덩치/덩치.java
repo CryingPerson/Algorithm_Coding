1990003683bba250cee4450849ab4ff989510579
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-03-06 15:41:30 +0900

    [Silver V] Title: 덩치, Time: 180 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,47 +1,35 @@
-
-import java.awt.*;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 import java.util.*;
 
-public class Main {
-    static int INF = Integer.MAX_VALUE;
-    static int[] ch;
-    static ArrayList<ArrayList<Integer>> list;
-    static int m;
-    static int n;
-    static int[][] board;
-    static int[][] dis;
-    static int[] answer;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] ans;
-    static int zero = 0;
-    static int[] arrs;
-    static int one = 0;
-    static StringBuilder sb = new StringBuilder();
+class Main {
+    public static int row;
+    public static char[][] board;
+    public static int col;
+
+    public static ArrayList<ArrayList<Integer>> list;
 
     public static void main(String[] args) {
 
         Scanner kb = new Scanner(System.in);
 
-        int n = kb.nextInt();
-        int[] rank = new int[n];
-        int[][] perSon = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                perSon[i][j] = kb.nextInt();
-            }
+        int T = kb.nextInt();
+
+        int[][] person = new int[T][2];
+        for (int i = 0; i < T; i++) {
+            person[i][0] = kb.nextInt();
+            person[i][1] = kb.nextInt();
         }
-        for (int i = 0; i < n; i++) {
-            rank[i] = 1;
-            for (int j = 0; j < n; j++) {
-                if(perSon[i][0] < perSon[j][0] && perSon[i][1] < perSon[j][1]){
-                    rank[i]++;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            int rank = 1;
+            for (int j = 0; j < T; j++) {
+                if(person[i][0] < person[j][0] &&
+                person[i][1] < person[j][1]){
+                    rank++;
                 }
             }
+            sb.append(rank).append(' ');
         }
-        for(int x : rank){
-            System.out.print(x + " ");
-        }
+        System.out.println(sb);
     }
 }
-
