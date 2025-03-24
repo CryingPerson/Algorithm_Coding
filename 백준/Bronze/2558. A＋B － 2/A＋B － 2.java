cf7f02640ec25810b356c7019b08d2c549824e1b
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-03-24 23:38:43 +0900

    [Bronze V] Title: A+B - 2, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int[][] board;
+
+    static int result = 0;
+    static int n;
+    static char[] chs;
+    static int max = -1;
+    static int[][] triangle;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
