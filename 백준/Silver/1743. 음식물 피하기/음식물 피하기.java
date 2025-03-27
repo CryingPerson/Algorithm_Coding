848a0c4a3765f1de110bdba04c0f3230ee75493a
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-03-27 15:52:53 +0900

    [Silver I] Title: 음식물 피하기, Time: 220 ms, Memory: 25772 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -1,30 +1,35 @@
+import javax.swing.plaf.PanelUI;
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 class Main {
     public static int n, m, k;
-    public static int[][] board;
+    public static ArrayList<ArrayList<Integer>> list;
+    public static boolean[] visited;
+    public static int[][]board;
     public static int cnt = 0;
-    public static int[] dx = {-1, 0, 1, 0};
-    public static int[] dy = {0, 1, 0, -1};
-
+    public static int[]dx = {-1,0,1,0};
+    public static int[]dy = {0,1,0,-1};
+    public static Set<String> st = new HashSet<>();
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        n = kb.nextInt();
-        m = kb.nextInt();
-        k = kb.nextInt();
+         n = kb.nextInt();
+         m = kb.nextInt();
+         k = kb.nextInt();
         board = new int[n][m];
 
         for (int i = 0; i < k; i++) {
-            int a = kb.nextInt() - 1;
-            int b = kb.nextInt() - 1;
+            int a = kb.nextInt() -1;
+            int b = kb.nextInt() -1;
+
             board[a][b] = 1;
         }
-
-        int maxSum = 0;
+        int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == 1) {
-                    int ans = BFS(i, j);
+                if(board[i][j] == 1){
+                    int ans = BFS(i,j);
                     maxSum = Math.max(maxSum, ans);
                     cnt = 0;
                 }
@@ -34,32 +39,34 @@ class Main {
     }
 
     public static int BFS(int x, int y) {
-        Queue<Point> queue = new LinkedList<>();
+        Queue<point> queue = new LinkedList<>();
         board[x][y] = 0;
         cnt = 1;
-        queue.add(new Point(x, y));
-
+        queue.add(new point(x,y));
         while (!queue.isEmpty()) {
-            Point poll = queue.poll();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                point poll = queue.poll();
 
-            for (int j = 0; j < 4; j++) {
-                int nx = poll.x + dx[j];
-                int ny = poll.y + dy[j];
+                for (int j = 0; j < 4; j++) {
+                    int nx = poll.x + dx[j];
+                    int ny = poll.y + dy[j];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 1) {
-                    cnt++;
-                    queue.add(new Point(nx, ny));
-                    board[nx][ny] = 0;
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 1){
+                        cnt++;
+                        queue.add(new point(nx,ny));
+                        board[nx][ny] = 0;
+                    }
                 }
             }
         }
         return cnt;
     }
 }
-
-class Point {
-    int x, y;
-    public Point(int x, int y) {
+class point{
+    int x;
+    int y;
+    public point(int x,int y) {
         this.x = x;
         this.y = y;
     }
