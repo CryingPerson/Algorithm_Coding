cd2a2608c7a5130fa873d7204632b2416ff31471
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-04-03 15:53:58 +0900

    [Silver II] Title: 유기농 배추, Time: 228 ms, Memory: 26500 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,47 +1,55 @@
-
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import javax.swing.plaf.PanelUI;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
 
 class Main {
-    static int[] dx = {-1, 0, 1, 0}; //
-    static int[] dy = {0, 1, 0, -1};
+    public static int n, x, y, k;
+    public static ArrayList<ArrayList<Integer>> list;
+    public static boolean[] visited;
+    public static int[][]board;
+    public static int cnt = 0;
+    public static int[]dx = {-1,0,1,0};
+    public static int[]dy = {0,1,0,-1};
+    public static Set<String> st = new HashSet<>();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+
         for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            int x = sc.nextInt(); int y = sc.nextInt(); int k =sc.nextInt();
-            int[][]board = new int[x][y];
+            x = kb.nextInt();
+            y = kb.nextInt();
+            k = kb.nextInt();
+            board = new int[x][y];
+
             for (int j = 0; j < k; j++) {
-                int a = sc.nextInt();
-                int b = sc.nextInt();
+                int row = kb.nextInt();
+                int col = kb.nextInt();
 
-                board[a][b] = 1;
+                board[row][col] = 1;
             }
-
-            for (int s = 0; s < x; s++) {
-                for (int p = 0; p < y; p++) {
-                    if(board[s][p] == 1) {
-                        cnt++;
-                        board[s][p] = 0;
-                        DFS(s,p,board,x,y);
+            int answer = 0;
+            for (int a = 0; a < x; a++) {
+                for (int b = 0; b < y; b++) {
+                    if(board[a][b] == 1){
+                        answer++;
+                        board[a][b] = 0;
+                        DFS(a,b);
                     }
                 }
             }
-            System.out.println(cnt);
+            System.out.println(answer);
         }
     }
 
-    public static void DFS(int n, int p, int[][] board, int mx, int my) {
+    public static void DFS(int xx, int yy) {
         for (int i = 0; i < 4; i++) {
-            int nx = dx[i] + n;
-            int ny = dy[i] + p;
+            int nx = xx + dx[i];
+            int ny = yy + dy[i];
 
-            if (nx >= 0 && nx < mx && ny >= 0 && ny < my && board[nx][ny] == 1) {
+            if (nx >= 0 && nx < x && ny >= 0 && ny < y && board[nx][ny] == 1) {
                 board[nx][ny] = 0;
-                DFS(nx,ny,board,mx,my);
+                DFS(nx,ny);
             }
         }
     }
