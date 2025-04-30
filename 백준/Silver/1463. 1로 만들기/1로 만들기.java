cd7424dbd527c03be1907b50de64dd289d5a6244
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-04-30 19:55:43 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 16924 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,20 @@
-import javax.xml.crypto.dsig.keyinfo.KeyInfo;
-import java.util.*;
+import java.util.Scanner;
 
-class Main {
-    public static int row;
-    public static int[][] board;
-    public static int col;
-    public static int sum = 0;
-    public static int white = 0;
-    public static int blue = 0;
-    public static int[] dx = {-1, 0, 1, 0};
-    public static int[] dy = {0, 1, 0, -1};
-
-    public static ArrayList<ArrayList<Integer>> list;
-
+public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner kb = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        int N = kb.nextInt();
-
-        int[] dp = new int[N+1];
+        int[]dp = new int[n+1];
 
         dp[1] = 0;
 
-        for (int i = 2; i <= N; i++) {
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i-1] + 1;
-            if(i % 2 == 0){
-                dp[i] = Math.min(dp[i/2] + 1, dp[i]);
-            }
-            if(i % 3 == 0){
-                dp[i] = Math.min(dp[i/3] + 1, dp[i]);
-            }
+            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+            if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
         }
-        System.out.println(dp[N]);
+        System.out.println(dp[n]);
     }
 }
