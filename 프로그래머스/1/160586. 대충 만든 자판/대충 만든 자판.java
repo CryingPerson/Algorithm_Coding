ef803d78baf5c333f9597904fa7b5f5f15455dfd
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-05-13 02:25:54 +0900

    [level 1] Title: 대충 만든 자판, Time: 29.89 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,46 +1,35 @@
 import java.util.*;
 class Solution {
-    static char ch;
     public int[] solution(String[] keymap, String[] targets) {
-        int len = targets.length;
-        int[] answer = new int[len];
+        int[] answer = new int[targets.length];
         
-        Map<Character, Integer> map = new HashMap();
-        for(String key : keymap){
-            for(int i = 0; i<key.length(); i++){
-                char ch = key.charAt(i);
-                
-                int value = map.getOrDefault(ch,i+1);
-                
-                map.put(ch, Math.min(value, i+1));
-            }
-        }
-        int index = 0;
-        for(String tg : targets){
-            for(int i = 0; i<tg.length(); i++){
-                if(map.containsKey(tg.charAt(i))){
-                    answer[index] += map.get(tg.charAt(i));
-                }else{
-                    answer[index] = -1;
-                    break;
+    
+        for(int i = 0; i < targets.length; i++){
+            int a = 0;
+   
+            for(int j = 0; j < targets[i].length(); j++){
+                char ch =  targets[i].charAt(j);
+                    int max = Integer.MAX_VALUE;
+                for(int k = 0; k < keymap.length; k++){
+                    int se = keymap[k].length() + 1;
+                    
+                    for(int q = 0; q < keymap[k].length(); q++){
+                        if(ch == keymap[k].charAt(q)){
+                            max = Math.min(max, q+1);
+                            se = q + 1;
+                            break;
+                        }
+                    }
                 }
+                if (max == Integer.MAX_VALUE){
+                  a = -1;
+                    break;
+                } 
+                a += max;
+                
             }
-            index++;
+           answer[i] = a;
         }
-        
-        
-        
         return answer;
     }
 }
-            // for(int i = 0; i<targets.length; i++){
-            //     for(int j = 0; j<targets[i].length(); j++){
-            //         char ch = targets[i].charAt(j);
-            //         if(map.containsKey(ch)){
-            //             answer[i] += map.get(ch);
-            //         }else{
-            //             answer[i] = -1;
-            //             break;
-            //         }
-            //     }                
-            // }
