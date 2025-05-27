cbc42236694460ec7702c55126c4c0aaf1dadb6a
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-05-27 19:05:15 +0900

    [level 2] Title: 할인 행사, Time: 105.22 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,27 +3,25 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        Map<String, Integer> food = new HashMap();
-        for(int i = 0; i < want.length; i++){
-            food.put(want[i], number[i]);
-        }
-        for(int i = 0; i < discount.length; i++){
-            HashMap<String, Integer> map = new HashMap();
-            
-            for(int j = i; j < i + 10 && i + 10 <= discount.length; j++){
-                map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
-            }
-            
+        Map<String, Integer> save = new HashMap();
+        
+        int idx = 0;
+        for(String food : want) save.put(food, number[idx++]);
+        
+        for(int i = 0; i <= discount.length - 10; i++){
+            Map<String, Integer> save2 = new HashMap();
             boolean flag = true;
-            
-            for(String foods : food.keySet()){
-                if(food.get(foods) != map.get(foods)) flag = false;
+            for(int j = i; j < i + 10; j++){
+                save2.put(discount[j], save2.getOrDefault(discount[j], 0) + 1);
             }
             
+            for(String val : save.keySet()){
+                if(save.get(val) != save2.get(val)){
+                    flag = false;
+                }
+            }
             if(flag) answer++;
         }
-        
-        
         return answer;
     }
 }
