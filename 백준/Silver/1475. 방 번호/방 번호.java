d87bf3fbdbde1e86788d941f8e082e89fbffea1a
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-06-09 21:46:51 +0900

    [Silver V] Title: 방 번호, Time: 92 ms, Memory: 12836 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,36 +1,39 @@
-import com.sun.jdi.Value;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static char[][] graph;
+    static boolean[][] visited;
+    static int[][] result;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int L = 0;
 
-        String s = br.readLine();
-        int[]arr = new int[10];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int[] arr = new int[11];
+        for (char ch : s.toCharArray()) {
+            if(ch =='6'){
+                arr['9' - '0']++;
+            }else{
+                arr[ch - '0']++;
+            }
+        }
         int max = -1;
-        for(int i = 0; i<s.length(); i++)
-        {
-            char ch = s.charAt(i);
-            if(ch == '9')
-            {
-                arr['6'-48]++;
+        int compare = -1;
+        for (int i = 0; i <= 10; i++) {
+            if (i == 6 || i == 9) {
+                compare = (int)Math.ceil((double) arr[i] / 2);
+                if (compare > max) max = compare;
+            } else {
+
+                compare = arr[i];
+                if (compare > max) max = compare;
+
             }
-            else
-            {
-                arr[ch-48]++;
-            }
-        }
-        if(arr[6] % 2 == 1) {
-            arr[6] = arr[6] / 2 + 1;
-        }else {
-            arr[6] = arr[6] / 2;
-        }
-        for(int i = 0; i<10; i++)
-        {
-            max = Math.max(max,arr[i]);
         }
         System.out.println(max);
     }
