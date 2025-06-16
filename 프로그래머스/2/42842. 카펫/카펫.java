f54122ffee5c677ef9e07cf1906f9529bf806a18
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-06-17 01:05:04 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,21 +2,18 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
         
-        int sum = brown + yellow;
+        int size = brown + yellow;
         
-        int sero = 0;
-        int garo = 0;
-        
-        for(int i = 1; i <= sum; i++){
-            if(sum % i == 0){
-                sero = sum / i;
-                
-                if(sero <= i){
-                    if((i - 2) * (sero -2) == yellow){
-                        return new int[]{i, sero};
-                    }
-                }
+        for(int i = 1; i <= brown; i++){
+            if(size % i == 0){
+                         int h = size / i;
+            if(i < h) continue;
+            
+            if((i - 2) * (h - 2) == yellow){
+                return new int[]{i, h};
+            }   
             }
+            
         }
         return answer;
     }
