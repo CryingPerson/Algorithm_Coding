da1ed11fe8c3fc5f7c1b72ffc9e1d73a41ae7cbf
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-06-16 16:08:28 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 20.58 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,26 +1,31 @@
 class Solution {
     public int solution(int[] mats, String[][] park) {
         int answer = 0;
-        
-        for(int x : mats){
-            for(int i = 0; i<park.length; i++){
-                for(int j = 0; j<park[i].length; j++){
-                    if(check(i,j,x,park)){
-                        answer = Math.max(x,answer);
+        for(int i = 0; i < park.length; i++){
+            for(int j = 0; j < park[0].length; j++){
+                int get = check(i, j, mats, park);
+                answer = Math.max(answer, get);
+            }
+        }
+        if(answer == 0) return - 1;
+        return answer;
+    }
+    static int check(int x, int y, int[] arr, String[][] arr2){
+        int ans = 0;
+        for(int a : arr){
+            boolean falg = true;
+            for(int i = x; i < x + a; i++){
+                for(int j = y; j < y + a; j++){
+                    if(i >= arr2.length ||j >= arr2[0].length || !arr2[i][j].equals("-1")){
+                        falg = false;
+                        break;
                     }
                 }
             }
-        }
-        return answer == 0 ? -1 : answer;
-    }
-    public static boolean check(int x, int y, int add, String[][]park){
-        for(int i =x; i<x+add; i++){
-            for(int j = y; j<y+add; j++){
-                if(i >= park.length || j >= park[0].length || !park[i][j].equals("-1")){
-                    return false;
-                }
+            if(falg){
+             ans = Math.max(ans, a);   
             }
         }
-        return true;
+        return ans;
     }
 }
