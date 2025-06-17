bdb268478713afb5df49c3665ad83b4771858817
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-06-18 00:42:20 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,22 +2,30 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        
+
         List<String> list = new ArrayList();
+        
         list.add(words[0]);
-        for(int i = 1; i<words.length; i++){
-            if(list.contains(words[i]) || same(list.get(i-1), words[i])){
-                answer[0] = i%n+1;
-                answer[1] = i/n+1;
-                break;
+        for(int i = 1; i < words.length; i++){
+            if(list.contains(words[i])){
+                int a = i % n + 1;
+                int b = i / n + 1;
+                
+                return new int[] {a,b};
+            }
+            if(same(words[i - 1], words[i])){
+                int a = i % n + 1;
+                int b = i / n + 1;
+                
+                return new int[] {a,b};
             }
             list.add(words[i]);
         }
 
         return answer;
     }
-    public static boolean same(String s, String s2){
-        char ch = s.charAt(s.length()-1);
+    static boolean same(String s, String s2){
+        char ch = s.charAt(s.length() - 1);
         char cs = s2.charAt(0);
         
         if(ch == cs) return false;
