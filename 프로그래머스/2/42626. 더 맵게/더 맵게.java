e54a15aadd8985756430850fa7ed8c04d0780a9a
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-06-25 01:33:05 +0900

    [level 2] Title: 더 맵게, Time: 1646.65 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,13 +3,27 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
-        PriorityQueue<Integer> pq = new PriorityQueue();
-        for(int x : scoville) pq.add(x);
-        boolean flag = false;
-        while(pq.peek() < K && pq.size() >= 2){
-            answer++;
-            pq.add(pq.poll() + (pq.poll() * 2));
+        PriorityQueue<Integer> q = new PriorityQueue();
+        
+        for(int x : scoville) q.add(x);
+        
+        boolean flag = true;
+        
+        while(q.size() > 1){
+            if(q.peek() >= K){
+                break;
+            }else{
+                int a = q.poll();
+                int b = q.poll();
+                int c = 0;
+                c = a + (b * 2);
+                q.add(c);
+                answer++;
+            }
         }
-      return (pq.peek() >= K) ? answer : -1;
+        if(q.poll() < K){
+            return -1;
+        }
+        return answer;
     }
 }
