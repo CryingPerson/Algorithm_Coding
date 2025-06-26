fa269db4897cd1d8bb87099c17fd796dbafe02a5
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-06-26 16:13:28 +0900

    [level 2] Title: 문자열 압축, Time: 3.76 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,38 +1,41 @@
 class Solution {
     public int solution(String s) {
         int answer = s.length();
-        
-        int sumLength = s.length();
-        
-        int half = sumLength/2;
-        
-        for(int unit = 1; unit <= half; unit++){
-            String pre = s.substring(0, unit);
+        int len = answer/2;
+        for(int unit = 1; unit <= len; unit++){
             StringBuilder sb = new StringBuilder();
+            String pre = s.substring(0, unit);
+            
             int count = 1;
-            for(int i = unit; i <= sumLength; i += unit){
-                String str;
-                if(i + unit <= sumLength){
-                    str = s.substring(i, i + unit);
+            for(int i = unit; i <= s.length(); i += unit){
+                String sub;
+                
+                if(i + unit <= s.length()){
+                    sub = s.substring(i, i+unit);
                 }else{
-                    str = s.substring(i);
+                    sub = s.substring(i);
                 }
-                if(str.equals(pre)){
+                
+                if(sub.equals(pre)){
                     count++;
                 }else{
                     if(count > 1){
-                     sb.append(count);   
+                        sb.append(count);
                     }
                     sb.append(pre);
-                    pre =str;
+                    pre = sub;
                     count = 1;
                 }
             }
             if(count > 1){
                 sb.append(count);
-            } sb.append(pre);
-            answer = Math.min(answer, sb.length());
+            }                sb.append(pre);
+
+            System.out.println(sb);
+            answer = Math.min(answer, sb.toString().length());
+
         }
+            
         return answer;
     }
 }
