b45c3452140b09b0c0fe2e3b902fb8bb9c068987
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-07-12 01:09:43 +0900

    [Bronze I] Title: 부재중 전화, Time: 92 ms, Memory: 12952 KB -BaekjoonHub

--- 백준/Bronze/1333. 부재중 전화/부재중 전화.java
+++ 백준/Bronze/1333. 부재중 전화/부재중 전화.java
@@ -1,34 +1,35 @@
 import java.util.*;
 
 public class Main {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt(); 
-        int length = sc.nextInt(); 
-        int bell = sc.nextInt();
+        int n = sc.nextInt(); int length = sc.nextInt(); int bell = sc.nextInt();
 
         int lastEmpty = (n - 1) * 5 + n * length - 1;
-
+        int start = 0;
+        int end = 0;
+        int a = bell;
         List<Integer> bells = new ArrayList<>();
-        for (int i = bell; i <= lastEmpty + 100; i += bell) {
+        for (int i = bell; i < lastEmpty + 100; i += bell) {
             bells.add(i);
         }
+        for(int i = 0; i < n; i ++) {
+            start = (i + 1) * length + i * 5;
+            end = start + 5;
 
-        for (int i = 0; i < n - 1; i++) {
-            int start = (i + 1) * length + i * 5;
-            int end = start + 5;
-
-            for (int num : bells) {
-                if (start <= num && num < end) {
+            for(int num : bells) {
+                if(start <= num && end > num) {
                     System.out.println(num);
                     return;
                 }
             }
         }
-
-        for (int s : bells) {
-            if (lastEmpty < s) {
+        for(int s : bells) {
+            if(lastEmpty < s){
                 System.out.println(s);
                 break;
             }
