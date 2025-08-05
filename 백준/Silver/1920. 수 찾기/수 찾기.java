81038269bf5e3157c9086fa5e2617a678d612f47
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-08-05 14:52:50 +0900

    [Silver IV] Title: 수 찾기, Time: 1888 ms, Memory: 186212 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -11,40 +11,18 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) map.put(sc.nextInt(), 1);
 
         int m = sc.nextInt();
 
-        Arrays.sort(arr);
         for (int i = 0; i < m; i++) {
-            int num = sc.nextInt();
-
-            if(check(arr, num)){
+            if(map.containsKey(sc.nextInt())){
                 System.out.println(1);
             }else{
                 System.out.println(0);
             }
         }
-    }
-
-    static boolean check(int[] arr, int n) {
-        int left = 0;
-        int right = arr.length-1;
-
-        int mid = -1;
-
-        while (left <= right) {
-
-            mid = (left + right) / 2;
-            if(arr[mid] == n) return true;
-
-            if(arr[mid] > n){
-                right = mid -1;
-            }else {
-                left = mid + 1;
-            }
-        }
-        return false;
     }
 }
