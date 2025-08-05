591228c991646a8efe24d9e818c992a6edf039b8
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-08-06 01:04:04 +0900

    [level 2] Title: [1차] 캐시, Time: 149.62 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 137 MB, 시간: 33.42 ms
+메모리: 115 MB, 시간: 149.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 20일 20:37:27
+2025년 08월 06일 01:04:02
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,32 +1,52 @@
 import java.util.*;
 class Solution {
-    public int solution(int cacheSize, String[] cities) {
+    public static int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        
-        Queue<String> save = new LinkedList();
-        String city2 = "";
+
+        HashMap<String, Integer> map = new HashMap();
+        HashMap<String, Integer> time = new HashMap();
+        int idx = 0;
+        int usaaa = 300000;
         for(String city : cities){
-            city2 = city.toLowerCase();
-            if(save.contains(city2)){
-
-                    save.remove(city2);
-                    save.add(city2);
+            city = city.toLowerCase();
+            if (cacheSize == 0) {
+                answer += 5;
+                continue;
+            }
+            if(map.size() != cacheSize){
+                if(map.containsKey(city)){
+                    time.put(city, usaaa++);
                     answer++;
-
-            }else{
-                if(cacheSize == 0){
-                    answer += 5;
                 }else{
-                    if(save.size() == cacheSize){
-                        save.poll();  
-                        save.add(city2);
-                    }else{
-                        save.add(city2);
-                    }
+                    time.put(city, usaaa++);
                     answer += 5;
+                }
+                map.put(city, map.getOrDefault(map.get(city) , 0) + 1);
+            }else{
+                if(map.containsKey(city)){
+                    map.put(city, map.getOrDefault(map.get(city) , 0) + 1);
+                    time.put(city, usaaa++);
+                    answer ++;
+                }else{
+                    int max = Integer.MAX_VALUE;
+                    String name = "";
+                    for(String cities2 : time.keySet()){
+                        if(max > time.get(cities2)){
+                            max = time.get(cities2);
+                            name = cities2;
+                        }
+                    }
+                    map.remove(name);
+                    time.remove(name);
+                    answer += 5;
+                    map.put(city, 1);
+                    time.put(city, usaaa++);
                 }
             }
         }
         return answer;
     }
 }
+
+// 5 5 5 1 1 1 1 1 1
+// 판교 서울 제주
