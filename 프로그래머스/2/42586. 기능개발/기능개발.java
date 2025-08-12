1b0cd06add8820fbd63c4a709e32853e3c3a8db5
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-08-13 00:06:20 +0900

    [level 2] Title: 기능개발, Time: 2.90 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,26 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        
-        Queue<Integer> q = new LinkedList();
-        List<Integer> li = new ArrayList();
-        for(int i = 0; i < progresses.length; i++){
-            int days = 100 - progresses[i];
-            
-            int time = (int)Math.ceil((double)days / speeds[i]);
-            q.add(time);
-        }
-        
-        while(!q.isEmpty()){
-            int cnt = 1;
-            int cur = q.poll();
-            while(!q.isEmpty() && q.peek() <= cur){
-                q.poll();
-                cnt++;
+        int[] answer = new int[progresses.length];
+     
+        int idx = 0;
+        int n = progresses.length;
+        List<Integer> list = new ArrayList();
+        while(idx < n){
+            for(int i = idx; i < progresses.length; i++){
+                progresses[i] += speeds[i];
             }
-            li.add(cnt);
+            
+            int cnt = 0;
+            
+            while(idx < n && progresses[idx] >= 100){
+                cnt++;
+                idx++;
+            }
+            
+            if(cnt != 0){
+                list.add(cnt);
+            }
         }
-        return li.stream().mapToInt(i -> i).toArray();
-    }
+        return list.stream().mapToInt(i -> i).toArray();
+    } 
 }
