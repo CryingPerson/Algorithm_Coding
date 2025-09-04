cb35db1a71c68662c556ef43afdb0241f3fb40b9
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-09-05 02:25:06 +0900

    [Silver IV] Title: 햄버거 사랑, Time: 212 ms, Memory: 19556 KB -BaekjoonHub

--- 백준/Silver/1980. 햄버거 사랑/햄버거 사랑.java
+++ 백준/Silver/1980. 햄버거 사랑/햄버거 사랑.java
@@ -9,26 +9,20 @@ public class Main {
         int save = -1;
         int min = Math.min(n,m);
         int max = Math.max(n,m);
-        int total = 0;
 
         List<info> list = new ArrayList<>();
-        if (t % min != 0) {
-            int tota = t / min;
-            int col = t % min;
-            list.add(new info(tota, col));
-        }
         if (t % min == 0) {
             save = Math.max(save, t / min);
             list.add(new info(save, 0));
         }else{
             int c = 0;
-            while (t >= 0) {
-                int cola = t % min;
-                int count = c + (t / min);
+            while (t > 0) {
+                int cola = t % min; int count = c + (t / min);
                 list.add(new info(count, cola));
                 t -= max;
                 c++;
             }
+
 
         }
         list.sort(((o1, o2) -> {
