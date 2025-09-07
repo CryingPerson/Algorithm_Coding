34839affe03777c2f11505d58afeff52badf1d53
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-09-07 19:56:57 +0900

    [Bronze V] Title: We love kriii, Time: 84 ms, Memory: 12732 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+}
