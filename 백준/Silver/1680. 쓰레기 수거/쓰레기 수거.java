da605cd38a8461e2f1e1e3e23c62473eafd9cb32
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-09-13 00:35:38 +0900

    [Silver III] Title: 쓰레기 수거, Time: 188 ms, Memory: 22444 KB -BaekjoonHub

--- 백준/Silver/1680. 쓰레기 수거/쓰레기 수거.java
+++ 백준/Silver/1680. 쓰레기 수거/쓰레기 수거.java
@@ -1,37 +1,49 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] board;
+    static int n;
+    static int cc = 1;
+    static List<List<Integer>> list = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            int totalLength = 0;
-            int maxWeight = sc.nextInt(); int place = sc.nextInt(); int continueLoength = 0; int curWeigh = 0;
-            int max = -1;
-            int cur = 0; int preLength = 0;
-            for (int j = 0; j < place; j++) {
-                int xLength = sc.nextInt();
-                int weigh = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
 
-                cur += weigh;
-                continueLoength += xLength - preLength;
+            int backup = W;
+            int total = 0;
+            int pre = 0;
 
-                if(cur == maxWeight){
-                    totalLength += 2 * continueLoength;
-                    cur = 0;
-                } else if (cur > maxWeight) {
-                    totalLength += 2 * continueLoength;
-                    cur = 0;
-                    cur += weigh;
+            for (int j = 0; j < N; j++) {
+                int len = sc.nextInt(); int weigh = sc.nextInt();
+
+                pre = len;
+
+                if (W < weigh) {
+                    total += 2 * len;
+                    W = backup;
                 }
 
-                preLength = xLength;
+                W -= weigh;
+
+                if (W == 0) {
+                    total += 2 * len;
+                    W = backup;
+                }
             }
-            if(cur != 0)totalLength += 2 * preLength
-                    ;
-            System.out.println(totalLength);
+            if (W != backup) {
+                total += 2 * pre;
+            }
+            System.out.println(total);
         }
+
     }
+
 }
