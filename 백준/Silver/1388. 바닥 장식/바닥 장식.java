9b5e7f7623357a16fbb9e29549e611a489420c46
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-09-14 19:19:55 +0900

    [Silver IV] Title: 바닥 장식, Time: 100 ms, Memory: 13052 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -1,47 +1,57 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class Main {
+    static char[][] board;
+    static int a1 = 0;
+    static int b1 = 0;
+    static boolean[][] booleans;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        char[][] tile = new char[a][b];
-        boolean[][] check = new boolean[a][b];
+        int x = sc.nextInt(); int y = sc.nextInt();
 
         sc.nextLine();
-        for (int i = 0; i < a; i++) {
-            String s = sc.nextLine();
-            for (int j = 0; j < b; j++) {
-                tile[i] = s.toCharArray();
+        board = new char[x][y];
+
+        booleans = new boolean[x][y];
+        for (int i = 0; i < x; i++) {
+            String lines = sc.nextLine();
+            for (int j = 0; j < y; j++) {
+                board[i][j] = lines.charAt(j);
             }
         }
+
         int cnt = 0;
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                if (!check[i][j]) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (!booleans[i][j]) {
+                    DFS(i,j, board[i][j]);
                     cnt++;
-                    DFS(i,j,tile,check);
                 }
             }
         }
         System.out.println(cnt);
     }
 
-    public static void DFS(int x, int y, char[][] ti, boolean[][] ch) {
-        ch[x][y] = true;
-        if(ti[x][y] == '-'){
-            int ny = y + 1;
-            if (ny < ti[0].length && ti[x][ny] == '-' && !ch[x][ny]) {
-                DFS(x,ny,ti,ch);
-            }
-        } else if (ti[x][y] == '|') {
-            int nx = x + 1;
-            if (nx < ti.length && ti[nx][y] == '|' && !ch[nx][y]) {
-                DFS(nx,y,ti,ch);
-            }
+    static void DFS(int x, int y, char ch) {
+        booleans[x][y] = true;
+        if(ch == '-'){
+            simul(x,y,ch,0,2);
+        }else{
+            simul(x,y,ch,2,4);
+        }
+    }
+
+    static void simul(int x, int y, char ch, int a, int b) {
+        for (int k = a; k < b; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+
+            if(nx < 0 || ny < 0 || nx >= board.length || ny >= board[0].length || booleans[nx][ny]) continue;
+            if(board[nx][ny] != ch) continue;
+            DFS(nx,ny,ch);
         }
     }
 }
