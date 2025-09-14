54990cc3c1682f62f7a35cceebabde7b354e69a0
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-09-14 16:51:06 +0900

    [Silver IV] Title: 나선, Time: 264 ms, Memory: 37572 KB -BaekjoonHub

--- 백준/Silver/1491. 나선/나선.java
+++ 백준/Silver/1491. 나선/나선.java
@@ -1,51 +1,43 @@
 import java.util.*;
 
 public class Main {
-    static int[] dx = {-1, 0, 1, 0};
+    static int[] dx = {1, 0 ,-1 ,0};
     static int[] dy = {0, 1, 0, -1};
-    static int aaaasd = 0;
-    static boolean[][] check;
-    static String[][] st = {{".",".",".","."}, {".",".",".","."}, {".",".",".","."}, {".",".",".","."}};
-    static String[][] st2 = {{"*","*","*","*"}, {".",".",".","."}, {".",".",".","."}, {".",".",".","."}};
-    static String[][] st3 = {{"*","*","*","*"}, {"*","*","*","*"}, {".",".",".","."}, {".",".",".","."}};
-    static String[][] st4 = {{"*","*","*","*"}, {"*","*","*","*"}, {"*","*","*","*"}, {".",".",".","."}};
-    static String[][] st5 = {{"*","*","*","*"}, {"*","*","*","*"}, {"*","*","*","*"}, {"*","*","*","*"}};
-    static int[] arr;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt(); int y = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
 
-        int[] dx = {1, 0 ,-1 ,0};
-        int[] dy = {0, 1, 0, -1};
+        int startX = 0;
+        int startY = 0;
 
-
-        int[][] board = new int[x][y];
-        boolean[][] checked = new boolean[x][y];
-        int dir = 0; int ax = 0; int ay = 0; int num = 1;
-        int findX = -1; int findY = -1;
+        int dir = 0;
         int total = x * y;
-        while (num <= total) {
-            board[ax][ay] = num;
-            checked[ax][ay] = true;
-            num++;
-            if(board[ax][ay] == total){
-                findX = ax;
-                findY = ay;
-                break;
-            }
-            int nx = ax + dx[dir];
-            int ny = ay + dy[dir];
+        int num = 1;
+        boolean[][] check = new boolean[x][y];
 
-            if (nx < 0 || nx >= x || ny < 0 || ny >= y || checked[nx][ny]) {
+        while (num < total) {
+
+            check[startX][startY] = true;
+
+            int nx = startX + dx[dir];
+            int ny = startY + dy[dir];
+
+            if (nx < 0 || ny < 0 || nx >= x || ny >= y || check[nx][ny]) {
                 dir = (dir + 1) % 4;
-                nx = ax + dx[dir];
-                ny = ay + dy[dir];
+                nx = startX + dx[dir];
+                ny = startY + dy[dir];
             }
 
-            ax = nx;
-            ay = ny;
+            startX = nx;
+            startY = ny;
+
+            num++;
+
         }
-        System.out.println(findX + " " + findY);
+
+        System.out.println(startX + " " + startY);
     }
 }
