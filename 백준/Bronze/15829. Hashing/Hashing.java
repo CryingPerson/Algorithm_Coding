7e62dc5ecfa9b38137245d79fed92ab1a9798bf1
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-09-17 01:10:44 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 13096 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,28 +1,51 @@
-import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    static ArrayList<ArrayList<Integer>> list1;
-    static int n, m;
+    static int a1 = 0;
+    static int b1 = 0;
+    static boolean active = true;
+    static int[] dx = {0, 1, 0 ,- 1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[] cx = {-1, -1, 1, 1};
+    static int[] cy = {-1, 1, 1, -1};
+    static boolean[] visited;
+    static int cnt = 0;
+    static int dir;
+    static int x;
+    static int y;
+    static boolean[][] snake;
+    static boolean[][] temp;
     static int[][] board;
-    static int MaxSave = -1;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
 
-    public static void main(String[] args) throws IOException {
+    public static int roomMax;
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
+
+        long count = 0;
+
+        int n = sc.nextInt();
+
+        BigInteger bigInteger = new BigInteger("31");
+
+        BigInteger sum = new BigInteger("0");
+        BigInteger bigInteger1 = new BigInteger("1234567891");
         sc.nextLine();
-        String s = sc.nextLine();
-        long result = 0;
+        String s= sc.nextLine();
         for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            int num = ch - 'a' + 1;
-            result += (long) num * (int)Math.pow(31, i);
+            char c = s.charAt(i);
+
+            int num = c - 'a' + 1;
+
+            BigInteger i1 = new BigInteger(String.valueOf(num));
+
+            BigInteger multiply = i1.multiply(bigInteger.pow(i));
+
+            sum = sum.add(multiply).mod(bigInteger1);
         }
-        System.out.println(result);
+        System.out.println(sum);
+
     }
 }
-
