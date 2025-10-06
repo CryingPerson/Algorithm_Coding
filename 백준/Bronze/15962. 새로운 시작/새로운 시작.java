42e052de0892f3381ee8ea7cc644cb593c6b596e
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-10-06 09:37:12 +0900

    [Bronze V] Title: 새로운 시작, Time: 84 ms, Memory: 12732 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("파이팅!!");
+    }
+}
