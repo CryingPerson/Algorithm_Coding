d47474ebb609ecdf43e10f210377810134ff67f3
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-10-10 16:51:21 +0900

    [Bronze V] Title: Hook, Time: 84 ms, Memory: 12732 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    static int[] dx = {1, -1, -1, 1};
+    static int[] dy = {1, 1, -1, -1};
+    static boolean[][] snake;
+    static int[][] board;
+    static int n;
+    static int count = 0;
+    static boolean[][] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
