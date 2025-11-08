026eeea59027b2a3e9b61fd311c5ac48eae84f3d
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-11-08 23:35:17 +0900

    [Gold IV] Title: 빙산, Time: 836 ms, Memory: 176688 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,104 +1,98 @@
-import java.awt.*;
 import java.util.*;
 
-class Main {
-    public static int row;
-    public static int[][] board;
-    public static int col;
-    public static int[]ch;
-    public static int wall = 0;
+public class Main {
+    static char[][] grid = new char[10][10];
 
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};
-
-    public static int N;
-    public static int n,m;
-    public static boolean[][] visited;
-    public static boolean[][] check;
-    public static ArrayList<ArrayList<Integer>> list;
-
+    static boolean[][] visited;
+    static int[][] board;
+    static int x, y, R;
+    static List<int[]> possible;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner kb = new Scanner(System.in);
+        x = sc.nextInt(); y = sc.nextInt();
+        board = new int[x][y];
 
-        n = kb.nextInt();
-        m = kb.nextInt();
-
-        board = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                board[i][j] = kb.nextInt();
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                board[i][j] = sc.nextInt();
             }
         }
 
-        System.out.println(simul());
-    }
-
-    public static int simul(){
-        int years = 0;
-
+        int day = 0;
         while (true) {
-            int count = DFS();
 
-            if(count >= 2) return years;
-            if(count == 0) return 0;
+            visited = new boolean[x][y];
+            possible = new LinkedList<>();
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    if(board[i][j] != 0){
+                        check(i, j);
+                    }
+                }
+            }
 
-            years++;
             disappear();
 
-        }
-    }
-    public static void disappear(){
-        int[][] temp = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if(board[i][j] > 0){
-                    int cnt = 0;
-                    for (int k = 0; k < 4; k++) {
-                        int nx = i + dx[k];
-                        int ny = j + dy[k];
-
-                        if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0){
-                            cnt++;
-                        }
+            int num = 0;
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    if (board[i][j] != 0 && !visited[i][j]) {
+                        countCheck(i, j);
+                        num++;
                     }
-                    temp[i][j] = cnt;
                 }
             }
-        }
+            if(num == 0) break;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                board[i][j] -= temp[i][j];
-                if(board[i][j] < 0) board[i][j] = 0; // 0보다 작지 않도록 처리
+            day++;
+
+            if(num > 1){
+                System.out.println(day);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+
+    static void countCheck(int xx, int yy) {
+        visited[xx][yy] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = xx + dx[i];
+            int ny = yy + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= x || ny >= y) continue;
+
+            if(!visited[nx][ny] && board[nx][ny] != 0){
+                countCheck(nx, ny);
             }
         }
     }
 
-    public static int DFS() {
-        int cnt = 0;
-        visited = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] > 0 && !visited[i][j]) {
-                    cnt++;
-                    dfs(i,j);
-                }
-            }
+    static void disappear() {
+        for (int[] pos : possible) {
+            if(board[pos[0]][pos[1]] - pos[2] < 0){
+                board[pos[0]][pos[1]] = 0;
+            }else board[pos[0]][pos[1]] -= pos[2];
         }
-        return cnt;
     }
-    static void dfs(int x, int y) {
-        visited[x][y] = true;
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
 
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && board[nx][ny] > 0) {
-                visited[nx][ny] = true;
-                dfs(nx, ny);
+    static void check(int cx, int cy) {
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= x || ny >= y) continue;
+
+            if(board[nx][ny] == 0){
+                count++;
             }
         }
+
+        possible.add(new int[]{cx,cy, count});
     }
 }
