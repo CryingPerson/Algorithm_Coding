1f18d8fda3095c5c2897c8630b7c37a727b6f068
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-11-11 15:47:58 +0900

    [Bronze II] Title: 시간복잡도를 배운 도도, Time: 484 ms, Memory: 55020 KB -BaekjoonHub

---
+++ 백준/Bronze/24510. 시간복잡도를 배운 도도/시간복잡도를 배운 도도.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        sc.nextLine();
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            int count = 0;
+             s= s.replace("for", "*").replace("while","*");
+
+            for(char c : s.toCharArray()) if(c == '*') count++;
+
+            if (count > max) {
+                max = count;
+            }
+        }
+        System.out.println(max);
+    }
+}
+
+
