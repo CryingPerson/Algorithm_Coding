3a839a3622fa7dc2903f9697733200bfc5cc773c
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-11-23 16:41:15 +0900

    [Silver IV] Title: 대피소, Time: 264 ms, Memory: 21536 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.java
+++ 백준/Silver/28215. 대피소/대피소.java
@@ -1,70 +1,76 @@
-import java.nio.IntBuffer;
 import java.util.*;
+import java.util.function.DoubleConsumer;
 
 public class Main {
-    static int[][] ans;
-    static int idx = 0;
-    static int max = Integer.MAX_VALUE;
-    static int x = Integer.MAX_VALUE;
-    static int y = 0;
-    static boolean[] check;
-    static List<int[]> list3;
-    static Set<String> set = new HashSet<>();
-    static String[][] ch = {{"q", "w", "e", "r", "t", "y", "u", "i", "o", "p"},
-            {"a", "s", "d", "f", "g", "h", "j", "k", "l", " "},
-            {"z", "x", "c", "v", "b", "n", "m", " ", " ", " "}};
-    static char first = 'R';
-
-
+    static int[] arr = new int[3];
+    static int x,y,z;
+    static int[][][] tomato;
+    static boolean[] visited;
+    static List<int[]> point = new LinkedList<>();
+    static List<int[]> avoidHome = new LinkedList<>();
+    static int[] dx = {-1 , 0 , 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static long min = Integer.MAX_VALUE;
+    static int k;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
 
-        list3 = new ArrayList<>();
-        check = new boolean[n];
-        idx = k;
-        List<int[]> list = new ArrayList<>();
+        int n = sc.nextInt();  k = sc.nextInt();
+
+        if(n == k){
+            System.out.println(0);
+            return;
+        }
+        visited = new boolean[n];
         for (int i = 0; i < n; i++) {
-            list.add(new int[]{sc.nextInt(), sc.nextInt()});
+            point.add(new int[]{sc.nextInt(), sc.nextInt()});
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            DFS(list, k, 0);
-        }
-        System.out.println(x);
+        DFS(0, 0);
+
+        System.out.println(min);
+
     }
 
-    static void DFS(List<int[]> list, int k, int L) {
-        if(L == k){
-            int reseach = reseach(list);
-            x = Math.min(reseach, x);
+    static void DFS(int l, int start) {
+        if (avoidHome.size() == k) {
+            min = Math.min(min, simi());
+            return;
         }
-        for (int i = 0; i < list.size(); i++) {
-            if(!check[i]){
-                check[i] = true;
-                list3.add(list.get(i));
-                DFS(list, k, L+1);
-                list3.remove(list3.size() - 1);
-                check[i] = false;
-            }
+
+        for (int i = start; i < point.size(); i++) {
+            avoidHome.add(point.get(i));
+            visited[i] = true;
+            DFS(l + 1, i + 1);
+            visited[i] = false;
+            avoidHome.remove(avoidHome.size() - 1);
         }
     }
 
-    static int reseach(List<int[]> list) {
-        int max = 0;
-        for (int[] li : list) {
-            int dis = Integer.MAX_VALUE;
-            int x = li[0];
-            int y = li[1];
+    static long simi() {
 
-            for (int[] ll : list3) {
-                int num = Math.abs(x - ll[0]) + Math.abs(y - ll[1]);
-                dis = Math.min(dis, num);
+
+
+
+        long max = Long.MIN_VALUE;
+        for (int i = 0; i < point.size(); i++) {
+            int[] pos = point.get(i);
+            if(visited[i]) continue;
+            int px = pos[0]; int py = pos[1];
+
+            long min = Long.MAX_VALUE;
+            for (int j = 0; j < avoidHome.size(); j++) {
+                int[] apos = avoidHome.get(j);
+
+                int ax = apos[0]; int ay = apos[1];
+
+                int distnce = Math.abs(px - ax) + Math.abs(py - ay);
+
+                min = Math.min(min, distnce);
+
             }
-            max = Math.max(max, dis);
+            max = Math.max(max ,min);
         }
         return max;
     }
 }
-
