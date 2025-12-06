de3bfc33099c49796bd7f0b964a902a02b176795
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-12-06 18:59:07 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 100 ms, Memory: 13384 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,62 +1,80 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    static int N;
-    static int[] ans;
-    public static void main(String[] args) throws IOException {
+class Main {
+    static int[][] board;
+    static int[][] origin;
+    static List<int[]> temp;
+
+    static int n,m,d;
+    static List<int[]> removePoint;
+    static int max = -1;
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-         N = sc.nextInt();
 
-         ans = new int[N+1];
-        for(int i = 1; i <= N; i++){
-            int sw = sc.nextInt();
-            ans[i] = sw;
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        int cnt = sc.nextInt();
-        for(int i = 0; i < cnt; i++){
-            int sex = sc.nextInt();
-            int num = sc.nextInt();
 
-            if(sex == 1){
-                change(num);
+        int m = sc.nextInt();
+
+        // 0 1 0 1 0 0 0 1
+        // 0 1 1 1 0 1 0 1
+        // 1 0 0 0 1 1 0 1
+        for (int i = 0; i < m; i++) {
+            int sex = sc.nextInt(); int num = sc.nextInt() - 1;
+
+            if (sex == 1) {
+                for(int j = num; j < arr.length; j += (num + 1)){
+                    if(arr[j] == 1){
+                        arr[j] = 0;
+                    }else arr[j] = 1;
+                }
             }else{
-                if(ans[num] == 1){
-                    ans[num] = 0;
-                }else ans[num] = 1;
-                girlSex(num);
+                boolean[] off = new boolean[n + 1];
+                int left = num - 1;
+                int right = num + 1;
+
+                off[num] = true;
+                while (left >= 0 && right < n) {
+                    if(arr[left] != arr[right]){
+                        break;
+                    }else{
+                        off[left] = true;
+                        off[right] = true;
+
+                        left--;
+                        right++;
+                    }
+                }
+                for (int j = 0; j < n; j++) {
+                    if(off[j]){
+                        if (arr[j] == 1) {
+                            arr[j] = 0;
+                        }else{
+                            arr[j] = 1;
+                        }
+                    }
+                }
+            }
+
+        }
+        int plus = 0;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append(" ");
+            plus++;
+            if (plus == 20) {
+                sb.append("\n");
+                plus = 0;
             }
         }
-        int count = 0;
-        for (int i = 1; i <= N; i++) {
-            count++;
-            System.out.print(ans[i] + " ");
-            if(count == 20){
-                System.out.println();
-                count = 0;
-            }
-        }
-    }
-    public static void change(int number){
-        for (int i = number; i <= N; i += number) {
-            ans[i] = 1 - ans[i];
-        }
-    }
-    public static void girlSex(int number){
-        int a = 1;
-        while (number - a > 0 && ans.length > number + a && ans[number-a] == ans[number+a] ){
-            if(ans[number-a] == 1){
-                ans[number-a] = 0;
-                ans[number+a] = 0;
-            }
-            else {
-                ans[number-a] = 1;
-                ans[number+a] = 1;
-            }
-            a++;
-        }
+        System.out.println(sb);
+
     }
 }
