8a2b1a42578b483c5446fcbbf4227882292a9de0
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-12-08 16:09:32 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 96 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class Main {
+    static String[][] board;
+    static int[][] origin;
+    static List<int[]> temp;
+    static boolean end = true;
+
+    static int Size = 0;
+    static int n, m, d;
+    static List<int[]> removePoint;
+    static int max = -1;
+
+    static List<int[]> teacher;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[]visited;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] zusci = new int[14];
+
+        int temp = n;
+        for(int i = 0; i < 14; i++) zusci[i] = sc.nextInt();
+
+        int jun = 0;
+        for (int i = 0; i < 14; i++) {
+            int curPrice = zusci[i];
+
+            if (curPrice <= n) {
+                jun += n / curPrice;
+                n %= curPrice;
+            }
+        }
+        int junPrice = jun * zusci[13];
+
+        if(n != 0) junPrice += n;
+
+        int sung = 0;
+
+        boolean masu = true;
+        boolean mado = true;
+        int save = temp;
+        int price = 0;
+        for (int i = 0; i < 11; i++) {
+            int one = zusci[i];
+            int two = zusci[i + 1];
+            int three = zusci[i + 2];
+
+            int four = zusci[i + 3];
+            if (one < two && two < three) {
+                save += sung * four;
+                sung = 0;
+            }else if(one > two && two > three){
+                sung += save / four;
+                save %= four;
+            }
+        }
+
+        price += save + sung * zusci[13];
+        if (junPrice > price) {
+            System.out.println("BNP");
+        } else if (junPrice < price) {
+            System.out.println("TIMING");
+        }else System.out.println("SAMESAME");
+    }
+}
