497167202888f84cd030959599fc0c079b97fbb8
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-12-09 23:58:33 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 356 ms, Memory: 147352 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,69 +1,117 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 class Main {
-    public static int[] ch;
-    public static int MinL = Integer.MAX_VALUE; // 최단 거리 초기화
-    public static int cnt = 0; // 최단 거리로 도달하는 방법의 개수
-    public static int m;
-    public static int[] dx = {-1, 1}; // 걷기 이동
+    static int[][] board;
+    static int[][] origin;
+    static List<int[]> temp;
+    static boolean end = true;
+
+    static int Size = 0;
+    static int n, m, d;
+    static boolean[][] che;
+    static List<int[]> removePoint;
+    static int max = -1;
+
+    static List<int[]> teacher;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[] visited;
+
+    static List<int[]> start;
 
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        m = kb.nextInt();
-        ch = new int[100001]; // 방문 시간을 저장하는 배열
 
-        BFS(n);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(MinL);
-        System.out.println(cnt);
+        n = sc.nextInt(); m = sc.nextInt();
+
+
+        int[] bfs = bfs();
+        int[] bfs1 = bfs(bfs[0]);
+
+        System.out.println(bfs[0]);
+        System.out.println(bfs1[0]);
     }
 
-    public static void BFS(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        ch[start] = 1; // 시작 위치의 방문 시간(1초) 설정
-        int L = 0;  // 현재 레벨(초 단위)
 
+    static int[] bfs(int nn) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(n);
+
+        int L = 0;
+        int count = 0;
+        int heu = Integer.MAX_VALUE;
+        visited = new boolean[100001];
+        visited[n] = true;
         while (!queue.isEmpty()) {
             int size = queue.size();
+
             for (int i = 0; i < size; i++) {
                 int poll = queue.poll();
-
-                // 동생 위치 찾았을 경우
-                if (poll == m) {
-                    if (MinL > L) {
-                        MinL = L;  // 최단 거리 갱신
-                        cnt = 1;   // 새로운 최단 거리 발견 → 방법 개수 초기화
-                    } else if (MinL == L) {
-                        cnt++;  // 같은 최단 거리 경로 추가
-                    }
+                visited[poll] = true;
+                if(poll == m && L == nn){
+                    count++;
                 }
+                int back = poll - 1;
 
-                // 걷기 이동
-                for (int j = 0; j < 2; j++) {
-                    int nx = poll + dx[j];
-                    if (nx >= 0 && nx <= 100000) {
-                        // 처음 방문하거나, 최단 거리와 같은 경우에만 탐색
-                        if (ch[nx] == 0 || ch[nx] == L + 1) {
-                            ch[nx] = L + 1; // 현재 이동 거리 저장
-                            queue.add(nx);
-                        }
-                    }
+                if(check(back)){
+                    queue.add(back);
                 }
-
-                // 순간이동 (X * 2)
-                int nx = poll * 2;
-                if (nx >= 0 && nx <= 100000) {
-                    if (ch[nx] == 0 || ch[nx] == L + 1) {
-                        ch[nx] = L + 1; // 현재 이동 거리 저장
-                        queue.add(nx);
-                    }
+                int front = poll + 1;
+                if(check(front)){
+                    queue.add(front);
+                }
+                int move = poll * 2;
+                if(check(move)){
+                    queue.add(move);
                 }
             }
             L++;
         }
+        return new int[]{count, 0};
+    }
+    static int[] bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(n);
+
+        int L = 0;
+        int count = 0;
+        int heu = Integer.MAX_VALUE;
+        visited = new boolean[100001];
+        visited[n] = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                int poll = queue.poll();
+                visited[poll] = true;
+                if(poll == m){
+                    heu = Math.min(L, heu);
+                }
+                int back = poll - 1;
+
+                if(check(back)){
+                    queue.add(back);
+                }
+                int front = poll + 1;
+                if(check(front)){
+                    queue.add(front);
+                }
+                int move = poll * 2;
+                if(check(move)){
+                    queue.add(move);
+                }
+            }
+            L++;
+        }
+        return new int[]{heu, 0};
+    }
+
+    static boolean check(int num) {
+        if(num < 0 || num > 100000) return false;
+        if(visited[num]) return false;
+        return true;
     }
 }
