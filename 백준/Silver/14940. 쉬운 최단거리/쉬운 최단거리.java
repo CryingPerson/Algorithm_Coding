617fa0d634d794af82613a36f1addd81c02dd372
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-12-09 15:37:44 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1096 ms, Memory: 218568 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,72 +1,95 @@
+import java.sql.ClientInfoStatus;
 import java.util.*;
 
-public class Main {
+class Main {
     static int[][] board;
-    static int[][] dis;
+    static int[][] origin;
+    static List<int[]> temp;
+    static boolean end = true;
+
+    static int Size = 0;
+    static int n, m, d;
+    static boolean[][] che;
+    static List<int[]> removePoint;
+    static int max = -1;
+
+    static List<int[]> teacher;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static Queue<point> queue;
+    static boolean[]visited;
 
+    static List<int[]> start;
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+
+        n = sc.nextInt(); m = sc.nextInt();
 
         board = new int[n][m];
-        dis = new int[n][m];
-        queue = new LinkedList<>();
 
+        che = new boolean[n][m];
+        start = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                int place = sc.nextInt();
-                board[i][j] = place;
-                if (place == 2) {
-                    queue.add(new point(i, j));
-                    dis[i][j] = 0;  // 목표지점은 거리가 0
-                } else {
-                    dis[i][j] = -1;  // 나머지 지점은 초기값 -1
-                }
+                board[i][j] = sc.nextInt();
+                if(board[i][j] == 0) che[i][j] = true;
+                if(board[i][j] == 2) start.add(new int[]{i,j});
             }
         }
 
-        BFS(n, m);
+        int startX = start.get(0)[0];
+        int stratY = start.get(0)[1];
 
-        // 결과 출력
+        bfs(startX, stratY);
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == 0) {
-                    System.out.print(0 + " ");  // 갈 수 없는 땅은 0
-                } else {
-                    System.out.print(dis[i][j] + " ");
-                }
+                if(!che[i][j]){
+                    sb.append("-1").append(" ");
+                }else sb.append(board[i][j]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 
-    public static void BFS(int n, int m) {
+    static void bfs(int inx, int iny) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.add(new int[]{inx,iny});
+
+
+        che[inx][iny] = true;
+
+
+        board[inx][iny] = 0;
+
+        int count = 1;
         while (!queue.isEmpty()) {
-            point poll = queue.poll();
+            int size = queue.size();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = poll.x + dx[i];
-                int ny = poll.y + dy[i];
+            for (int j = 0; j < size; j++) {
+                int[] poll = queue.poll();
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 1 && dis[nx][ny] == -1) {
-                    dis[nx][ny] = dis[poll.x][poll.y] + 1;
-                    queue.add(new point(nx, ny));
+                int cx = poll[0];
+                int cy = poll[1];
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = dx[i] + cx;
+                    int ny = dy[i] + cy;
+
+                    if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+
+                    if(board[nx][ny] == 0) continue;
+                    if(che[nx][ny]) continue;
+
+                    board[nx][ny] = count;
+                    queue.add(new int[]{nx,ny});
+                    che[nx][ny] = true;
                 }
             }
+            count++;
         }
-    }
-}
-
-class point {
-    int x;
-    int y;
-
-    public point(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
