08cc6d3d7372ec3ac5b1603e9da7d3bc49577f27
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-12-24 12:13:10 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 2544 ms, Memory: 326808 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+class Main {
+    static int max = Integer.MIN_VALUE;
+
+    static int hihi = -1;
+    static boolean[] visited;
+    static List<List<Integer>> list = new ArrayList<>();
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); int m = sc.nextInt(); int start = sc.nextInt();
+
+        visited = new boolean[n + 1];
+        arr = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+
+            list.get(from).add(to);
+            list.get(to).add(from);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            Collections.sort(list.get(i));
+        }
+
+        visited[start] = true;
+        arr[start] = 1;
+        dfs(start, 1);
+
+        for (int i = 1; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    static void dfs(int start, int plus) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+
+            for(int num : list.get(poll)){
+                if (!visited[num]) {
+                    plus++;
+                    visited[num] = true;
+                    arr[num] = plus;
+                    queue.add(num);
+                }
+            }
+        }
+
+
+    }
+}
