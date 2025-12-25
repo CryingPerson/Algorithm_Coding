c9f3bab0e533e743e1483700dc211626184d05d0
이시현 <134962465+CryingPerson@users.noreply.github.com>
2025-12-25 17:39:59 +0900

    [Silver IV] Title: 상근이의 여행, Time: 2312 ms, Memory: 235732 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[][] board;
+
+    static int count = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            List<List<Integer>> list = new ArrayList<>();
+
+            for (int j = 0; j <= N; j++) {
+                list.add(new ArrayList<>());
+            }
+
+            for (int j = 0; j < M; j++) {
+                int from = sc.nextInt();
+                int to = sc.nextInt();
+
+                list.get(from).add(to);
+                list.get(to).add(from);
+            }
+
+            int min = Integer.MAX_VALUE;
+            boolean[] visited;
+            for (int j = 1; j <= N; j++) {
+                count = 0;
+                visited = new boolean[N+1];
+                tree(visited, j, list);
+                min = Math.min(min, count);
+            }
+            System.out.println(min);
+        }
+    }
+
+    static void tree(boolean[] visited, int start, List<List<Integer>> list) {
+        visited[start] = true;
+        for(int next : list.get(start)){
+            if(!visited[next]){
+                count++;
+                tree(visited, next, list);
+            }
+        }
+    }
+}
