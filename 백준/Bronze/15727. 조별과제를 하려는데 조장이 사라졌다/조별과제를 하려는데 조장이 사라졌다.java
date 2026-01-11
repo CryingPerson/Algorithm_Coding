d1ec62575269879f7c5dc2886c53812ad70c1aba
이시현 <134962465+CryingPerson@users.noreply.github.com>
2026-01-11 17:01:39 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 96 ms, Memory: 13020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class Main {
+    static int n,m,l;
+    static int[][] a;
+    static int up = -1, down = -1;
+
+    static final int[] dr = {-1, 1, 0, 0};
+    static final int[] dc = {0, 0, -1, 1};
+
+
+    static int min = Integer.MAX_VALUE;
+    static int max = Integer.MIN_VALUE;
+
+    static int[] number;
+    static boolean[] visited;
+    static boolean[] nes;
+    static List<Character> math;
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int ans = n % 5 == 0 ? n / 5 : n / 5 + 1;
+
+        System.out.println(ans);
+    }
+
+}
