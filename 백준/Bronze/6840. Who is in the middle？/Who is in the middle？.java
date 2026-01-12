b6da05470a2113c03d429327c14c1afa283c26a3
이시현 <134962465+CryingPerson@users.noreply.github.com>
2026-01-12 18:02:05 +0900

    [Bronze V] Title: Who is in the middle?, Time: 108 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class Main {
+    static int n,m,l;
+    static int[][] a;
+    static int up = -1, down = -1;
+
+    static final int[] dr = {-1, 1, 0, 0};
+    static final int[] dc = {0, 0, -1, 1};
+
+
+    static int min = Integer.MAX_VALUE;
+    static int max = Integer.MIN_VALUE;
+
+    static int[] number;
+    static boolean[] visited;
+    static boolean[] nes;
+    static List<Character> math;
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[3];
+        
+        for(int i = 0; i < 3; i++) arr[i] = sc.nextInt(); 
+        Arrays.sort(arr);
+
+        System.out.println(arr[1]);
+    }
+
+}
