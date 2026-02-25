d729781039bb77639f05c34f3489e73432560d90
이시현 <134962465+CryingPerson@users.noreply.github.com>
2026-02-25 23:55:33 +0900

    [Gold III] Title: 치즈, Time: 376 ms, Memory: 42160 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -1,78 +1,90 @@
 import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static boolean[] isBroken = new boolean[10];
+    static int minTotalPress = Integer.MIN_VALUE;
 
-    static int x,y;
     static int[][] board;
-    static List<info> union;
-    static boolean[][] visited;
-    static int[] dx = {-1 ,0 ,1, 0};
+    static List<int[]> cheeze;
+    static List<int[]> store;
+    static List<List<Integer>> list;
+    static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static Map<List<Integer>, Integer> map;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        x = sc.nextInt(); y = sc.nextInt(); board = new int[x][y];
-        union = new ArrayList<>();
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
+        n = sc.nextInt(); m = sc.nextInt();
+
+        board = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 board[i][j] = sc.nextInt();
             }
         }
 
         int day = 0;
         while (true) {
-            boolean end = false;
 
-            visited = new boolean[x][y];
-            map = new HashMap<>();
-            DFS(0, 0);
-            if (map.size() == 0) {
-                end = true;
+            cheeze = new ArrayList<>();
+            store = new ArrayList<>();
+            deploy(0, 0);
+
+            if(store.size() == 0){
+                break;
             }
-            if(end) break;
-            for (Map.Entry<List<Integer>, Integer> m : map.entrySet()) {
-                if (m.getValue() >= 2) {
-                    List<Integer> key = m.getKey();
-                    board[key.get(0)][key.get(1)] = 0;
-                }
+
+            for (int[] xx : store) {
+                board[xx[0]][xx[1]] = 0;
             }
             day++;
+
         }
+
         System.out.println(day);
     }
 
-    static void DFS(int ax, int ay) {
-        visited[ax][ay] = true;
-        for (int i = 0; i < 4; i++) {
-            int nx = ax + dx[i];
-            int ny = ay + dy[i];
+    static void deploy(int x, int y) {
+        Queue<int[]> queue = new ArrayDeque<>();
 
-            if(nx < 0 || ny < 0 || nx >= x || ny >= y) continue;
+        queue.add(new int[]{x, y});
 
-            if(visited[nx][ny]) continue;
+        boolean[][] visited = new boolean[n][m];
+        int[][] possble = new int[n][m];
+        visited[x][y] = true;
 
-            if(board[nx][ny] == 1){
-                List<Integer> nx1 = List.of(nx, ny);
-                map.put(nx1, map.getOrDefault(nx1, 0) + 1);
-            }else{
-                DFS(nx,ny);
+        while (!queue.isEmpty()) {
+
+            int[] cur = queue.poll();
+
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >=m)continue;
+                if(visited[nx][ny]) continue;
+
+                if(board[nx][ny] == 1){
+                    possble[nx][ny]++;
+                }else {
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx,ny});
+                }
+            }
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (possble[i][j] >= 2) {
+                    store.add(new int[]{i,j});
+                }
             }
         }
-    }
-}
-class info{
-    int xxx;
-    int yyy;
-
-    public info(int xxx, int yyy) {
-        this.xxx = xxx;
-        this.yyy = yyy;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(xxx, yyy);
     }
 }
