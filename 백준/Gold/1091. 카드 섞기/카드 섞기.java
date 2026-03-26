239a92fde49036ca74aba7b1284c81cf03cd3dfc
이시현 <134962465+CryingPerson@users.noreply.github.com>
2026-03-26 22:44:24 +0900

    [Gold IV] Title: 카드 섞기, Time: 268 ms, Memory: 216412 KB -BaekjoonHub

---
+++ 백준/Gold/1091. 카드 섞기/카드 섞기.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class Main {
+    static int T;
+    static List<Integer>[][] gears;
+
+    static int[] dist;
+    static int n;
+    static List<List<node>> list = new ArrayList<>();
+    static int[] P;
+    static int[] S;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+         n = sc.nextInt();
+
+         P = new int[n];
+         S = new int[n];
+        for (int i = 0; i < n; i++) {
+            P[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            S[i] = sc.nextInt();
+        }
+
+
+        int cnt = 0;
+        int[] temp = P.clone();
+        int max = 1000000;
+        while (true) {
+            if(check(temp)){
+                break;
+            }
+
+            int[] mix = mix(temp);
+
+            temp = mix.clone();
+            cnt++;
+
+            if(cnt > 500000){
+                System.out.println(-1);
+                return;
+            }
+
+        }
+
+        System.out.println(cnt);
+    }
+
+    static boolean check(int[] t) {
+        int count = 0;
+        for (int i = 0; i < t.length; i++) {
+
+            int num = t[i];
+            int idx = i % 3;
+
+            if(num != idx) return false;
+        }
+
+        return true;
+    }
+
+    static int[] mix(int[] temp) {
+
+        int[] te = new int[n];
+        for (int i = 0; i < S.length; i++) {
+            int num = temp[i];
+            int swith = S[i];
+
+            te[swith] = num;
+        }
+        return te;
+    }
+
+}
+
+class node{
+    int to;
+    int cost;
+
+    public node(int to, int cost) {
+        this.to = to;
+        this.cost = cost;
+    }
+}
